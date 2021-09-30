1fa38dd40e15ef02d3dadbae5e2f2c1c417637af
GekoTree <91695181+GekoTree@users.noreply.github.com>
2021-10-01 00:44:43 +0530

    Add files via upload

---
+++ Github Finder/github.js
@@ -0,0 +1,22 @@
+class Github {
+    constructor() {
+      this.client_id = 'd97fbdbbc13e9a56a5e4';
+      this.client_secret = '89aed40d4a4cf132c8c60cf443aa088d34a5be7f';
+      this.repos_count = 5;
+      this.repos_sort = 'created: asc';
+    }
+  
+    async getUser(user) {
+      const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
+  
+      const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
+  
+      const profile = await profileResponse.json();
+      const repos = await repoResponse.json();
+  
+      return {
+        profile,
+        repos
+      }
+    }
+  }
