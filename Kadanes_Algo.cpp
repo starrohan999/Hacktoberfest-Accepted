0580d45cd8b7bf6970a0b50727e3859f47b98f61
FinalGetsugaa <39957448+FinalGetsugaa@users.noreply.github.com>
2021-10-10 19:08:00 +0530

    Create Kadanes_Algo.cpp

---
+++ Kadanes_Algo.cpp
@@ -0,0 +1,46 @@
+// Problem - Write an efficient program to find the sum of contiguous subarray within a one-dimensional array of numbers that has the largest sum.
+
+// ==== Kadane's Algo ====
+Initialize:
+    max_so_far = INT_MIN
+    max_ending_here = 0
+
+// Loop for each element of the array
+//   (a) max_ending_here = max_ending_here + a[i]
+//   (b) if(max_so_far < max_ending_here)
+//             max_so_far = max_ending_here
+//   (c) if(max_ending_here < 0)
+//             max_ending_here = 0
+// return max_so_far
+      
+      
+// ===== Implementation of Kadanes Algo =======
+#include<iostream>
+#include<climits>
+using namespace std;
+
+int maxSubArraySum(int a[], int size)
+{
+	int max_so_far = INT_MIN, max_ending_here = 0;
+
+	for (int i = 0; i < size; i++)
+	{
+		max_ending_here = max_ending_here + a[i];
+		if (max_so_far < max_ending_here)
+			max_so_far = max_ending_here;
+
+		if (max_ending_here < 0)
+			max_ending_here = 0;
+	}
+	return max_so_far;
+}
+
+/*Driver program to test maxSubArraySum*/
+int main()
+{
+	int a[] = {-2, -3, 4, -1, -2, 1, 5, -3};
+	int n = sizeof(a)/sizeof(a[0]);
+	int max_sum = maxSubArraySum(a, n);
+	cout << "Maximum contiguous sum is " << max_sum;
+	return 0;
+}
