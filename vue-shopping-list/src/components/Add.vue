ba262fd48b31eea2e6d3915b6defd7693c25fb9b
Fernando Merino <fermervil@gmail.com>
2021-10-20 19:23:03 +0200

    Add Vue shopping list

---
+++ vue-shopping-list/src/components/Add.vue
@@ -0,0 +1,24 @@
+<template >
+   <input type="text" v-model='current' @keyup.enter='add' >
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      current: null,
+    };
+  },
+  methods: {
+    add() {
+      this.$emit('addElement', this.current);
+      this.current = null;
+    },
+  },
+  name: 'add',
+};
+</script>
+
+<style scoped>
+
+</style>
---
+++ vue-shopping-list/src/components/User.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class='user'>
+    <img :src="user.avatar" alt="">
+    <div> {{user.first_name}} </div>
+    <div v-if='surname'> {{user.last_name}} </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    user: {
+      type: Object,
+      required: true,
+    },
+    surname: {
+      type: Boolean,
+    },
+  },
+  name: 'User',
+};
+</script>
+<style scope>
+.user {
+  display: flex;
+  align-content: space-between;
+  align-items: center;
+}
+</style>
