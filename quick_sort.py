d0b868048f04abd04bcc6dd8e11c0f025d371ded
suksome <sakshamsai26sep@gmail.com>
2022-10-12 21:15:20 +0530

    Added quick_sort.py

---
+++ quick_sort.py
@@ -0,0 +1,17 @@
+def sort(array):
+    less = []
+    equal = []
+    greater = []
+
+    if len(array) > 1:
+        pivot = array[0]
+        for x in array:
+            if x < pivot:
+                less.append(x)
+            elif x == pivot:
+                equal.append(x)
+            elif x > pivot:
+                greater.append(x)
+        return sort(less)+equal+sort(greater) 
+    else: 
+        return array
